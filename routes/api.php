a0cd1dd7e01b073a3c9c6f0edc49f9c052f29486
seunex17 <seunexseun@gmail.com>
2024-04-06 15:51:10 +0100

    Missing login route

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function() {
     Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
 });
 
 Route::middleware('auth:sanctum')->prefix('ticket')->group(function() {
